d172e145bb339963ab9575ce797123d34eadc4d4
mangeshchavan0022 <mangeshchavan0022@gmail.com>
2025-12-05 00:35:55 +0530

    Added line

--- service/PGMProjectSolution.Domain/Dto/AspNetRoleDto.cs
+++ service/PGMProjectSolution.Domain/Dto/AspNetRoleDto.cs
@@ -4,7 +4,7 @@ namespace PGMProjectSolution.Domain.DTO
     public class AspNetRoleDto
     {
         public string RoleId { get; set; }
-
+        
         public string UserName { get; set; }
     }
 }
