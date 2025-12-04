6e6ba179dace7ee275f227f1bb25d74aa09e3a81
mangeshchavan0022 <mangeshchavan0022@gmail.com>
2025-12-05 00:28:22 +0530

    Added Files

--- service/PGMProjectSolution.Domain/Dto/AspNetPgOwner.cs
+++ service/PGMProjectSolution.Domain/Dto/AspNetPgOwner.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace PGMProjectSolution.Domain.Dto
 {
     public class AspNetPgOwner
